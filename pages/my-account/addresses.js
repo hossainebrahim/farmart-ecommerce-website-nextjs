860fda6a8445def3259bd364c19e026aa0fc494e
Ebrahim <hossainebrahim03@gmail.com>
2022-11-02 22:27:38 +0600

    addresses update

--- pages/my-account/addresses.js
+++ pages/my-account/addresses.js
@@ -1,8 +1,13 @@
 import React from "react";
+import AddressForm from "../../app/components/dashboard/AddressForm";
 import DashboardLayout from "../../app/layout/DashboardLayout";
 
 const Addresses = () => {
-  return <DashboardLayout>Addresses</DashboardLayout>;
+  return (
+    <DashboardLayout>
+      <AddressForm />
+    </DashboardLayout>
+  );
 };
 
 export default Addresses;
